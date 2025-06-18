1fb47b7f7caba9c4e2416b7de83d533d70b10ea3
shukurergashov <shukurillo.dev@gmail.com>
2025-06-18 11:26:31 -0400

    Initial commit: add project structure, README, and .gitignore

---
+++ src/main/java/com/timetrak/TimeTrakApplication.java
@@ -0,0 +1,13 @@
+package com.timetrak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeTrakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeTrakApplication.class, args);
+    }
+
+}
